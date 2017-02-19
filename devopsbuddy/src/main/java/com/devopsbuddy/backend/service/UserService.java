41806f41f8e879de7d164e57dab281b6c3ce820f
John Popilek <john.popilek@gmail.com>
2017-02-19 16:42:24 -0600

    Added password encryption to spring security

--- devopsbuddy/src/main/java/com/devopsbuddy/backend/service/UserService.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/backend/service/UserService.java
@@ -3,6 +3,7 @@ package com.devopsbuddy.backend.service;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,35 +22,41 @@ import com.devopsbuddy.enums.PlansEnum;
 @Transactional(readOnly = true)
 public class UserService {
 
-	@Autowired
-	private RoleRepository roleRepository;
+  @Autowired
+  private RoleRepository roleRepository;
 
-	@Autowired
-	private PlanRepository planRepository;
+  @Autowired
+  private PlanRepository planRepository;
 
-	@Autowired
-	private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-	@Transactional
-	public User createUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles) {
+  @Autowired
+  private BCryptPasswordEncoder passwordEncoder;
 
-		Plan plan = new Plan(plansEnum);
-		// It makes sure the plans exist in the database
-		if (!planRepository.exists(plansEnum.getId())) {
-			plan = planRepository.save(plan);
-		}
+  @Transactional
+  public User createUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles) {
 
-		user.setPlan(plan);
+    String encryptedPassword = passwordEncoder.encode(user.getPassword());
+    user.setPassword(encryptedPassword);
 
-		for (UserRole ur : userRoles) {
-			roleRepository.save(ur.getRole());
-		}
+    Plan plan = new Plan(plansEnum);
+    // It makes sure the plans exist in the database
+    if (!planRepository.exists(plansEnum.getId())) {
+      plan = planRepository.save(plan);
+    }
 
-		user.getUserRoles().addAll(userRoles);
+    user.setPlan(plan);
 
-		user = userRepository.save(user);
+    for (UserRole ur : userRoles) {
+      roleRepository.save(ur.getRole());
+    }
 
-		return user;
+    user.getUserRoles().addAll(userRoles);
 
-	}
+    user = userRepository.save(user);
+
+    return user;
+
+  }
 }
--- devopsbuddy/src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -1,15 +1,18 @@
 package com.devopsbuddy.config;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.devopsbuddy.backend.service.UserSecurityService;
 
@@ -20,32 +23,40 @@ import com.devopsbuddy.backend.service.UserSecurityService;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserSecurityService userSecurityService;
+  @Autowired
+  private UserSecurityService userSecurityService;
 
-	@Autowired
-	private Environment env;
+  @Autowired
+  private Environment env;
 
-	/** Public URLs. */
-	private static final String[] PUBLIC_MATCHERS = { "/webjars/**", "/css/**", "/js/**", "/images/**", "/",
-			"/about/**", "/contact/**", "/error/**/*", "/console/**" };
+  /** The encryption SALT. */
+  private static final String SALT = "fdalkjalk;3jlwf00sfaof";
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+  }
 
-		List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
-		if (activeProfiles.contains("dev")) {
-			http.csrf().disable();
-			http.headers().frameOptions().disable();
-		}
+  /** Public URLs. */
+  private static final String[] PUBLIC_MATCHERS = {"/webjars/**", "/css/**", "/js/**", "/images/**",
+      "/", "/about/**", "/contact/**", "/error/**/*", "/console/**"};
 
-		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").defaultSuccessUrl("/payload").failureUrl("/login?error").permitAll().and().logout()
-				.permitAll();
-	}
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userSecurityService);
-	}
+    List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+    if (activeProfiles.contains("dev")) {
+      http.csrf().disable();
+      http.headers().frameOptions().disable();
+    }
+
+    http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated()
+        .and().formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+        .failureUrl("/login?error").permitAll().and().logout().permitAll();
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+  }
 }
