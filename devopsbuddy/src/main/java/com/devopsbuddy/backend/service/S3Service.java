07811bb8fa19390dd0f2f72fb5bb54fca2ecd052
John Popilek <john.popilek@gmail.com>
2017-03-18 21:20:49 -0500

    Exception handling and code clean up

--- devopsbuddy/src/main/java/com/devopsbuddy/backend/service/S3Service.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/backend/service/S3Service.java
@@ -1,11 +1,10 @@
 package com.devopsbuddy.backend.service;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.AccessControlList;
-import com.amazonaws.services.s3.model.GroupGrantee;
-import com.amazonaws.services.s3.model.Permission;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,10 +13,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.GroupGrantee;
+import com.amazonaws.services.s3.model.Permission;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.devopsbuddy.exceptions.S3Exception;
 
 /**
  * Created by tedonema on 08/05/2016.
@@ -44,45 +46,47 @@ public class S3Service {
 
 
     /**
-     * It stores the given file name in S3 and returns the key under which the file has been stored
-     * @param uploadedFile The multipart file uploaed by the user
-     * @param username The username for which to upload this file
-     * @return The URL of the uploaded image
-     */
-    public String storeProfileImage(MultipartFile uploadedFile, String username) throws
-            IOException {
+   * It stores the given file name in S3 and returns the key under which the file has been stored
+   * 
+   * @param uploadedFile The multipart file uploaed by the user
+   * @param username The username for which to upload this file
+   * @return The URL of the uploaded image
+   * @throws S3Exception If something goes wrong
+   */
+  public String storeProfileImage(MultipartFile uploadedFile, String username) {
 
         String profileImageUrl = null;
 
-        if (uploadedFile != null && !uploadedFile.isEmpty()) {
-            byte[] bytes = uploadedFile.getBytes();
+    try {
+      if (uploadedFile != null && !uploadedFile.isEmpty()) {
+        byte[] bytes = uploadedFile.getBytes();
 
-            // The root of our temporary assets. Will create if it doesn't exist
-            File tmpImageStoredFolder = new File(tempImageStore + File.separatorChar + username);
-            if (!tmpImageStoredFolder.exists()) {
-                LOG.info("Creating the temporary root for the S3 assets");
-                tmpImageStoredFolder.mkdirs();
-            }
+        // The root of our temporary assets. Will create if it doesn't exist
+        File tmpImageStoredFolder = new File(tempImageStore + File.separatorChar + username);
+        if (!tmpImageStoredFolder.exists()) {
+          LOG.info("Creating the temporary root for the S3 assets");
+          tmpImageStoredFolder.mkdirs();
+        }
 
-            // The temporary file where the profile image will be stored
-            File tmpProfileImageFile = new File(tmpImageStoredFolder.getAbsolutePath()
-                    + File.separatorChar
-                    + PROFILE_PICTURE_FILE_NAME
-                    + "."
-                    + FilenameUtils.getExtension(uploadedFile.getOriginalFilename()));
+        // The temporary file where the profile image will be stored
+        File tmpProfileImageFile = new File(
+            tmpImageStoredFolder.getAbsolutePath() + File.separatorChar + PROFILE_PICTURE_FILE_NAME
+                + "." + FilenameUtils.getExtension(uploadedFile.getOriginalFilename()));
 
-            LOG.info("Temporary file will be saved to {}", tmpProfileImageFile.getAbsolutePath());
+        LOG.info("Temporary file will be saved to {}", tmpProfileImageFile.getAbsolutePath());
 
-            try(BufferedOutputStream stream =
-                        new BufferedOutputStream(
-                                new FileOutputStream(new File(tmpProfileImageFile.getAbsolutePath())))) {
-                stream.write(bytes);
-            }
+        try (BufferedOutputStream stream = new BufferedOutputStream(
+            new FileOutputStream(new File(tmpProfileImageFile.getAbsolutePath())))) {
+          stream.write(bytes);
+        }
 
-            profileImageUrl = this.storeProfileImageToS3(tmpProfileImageFile, username);
+        profileImageUrl = this.storeProfileImageToS3(tmpProfileImageFile, username);
 
-            // Clean up the temporary folder
-            tmpProfileImageFile.delete();
+        // Clean up the temporary folder
+        tmpProfileImageFile.delete();
+      }
+    } catch (IOException e) {
+      throw new S3Exception(e);
         }
 
         return profileImageUrl;
@@ -92,11 +96,15 @@ public class S3Service {
     //--------------> Private methods
 
     /**
-     * Returns the root URL where the bucket name is located.
-     * <p>Please note that the URL does not contain the bucket name</p>
-     * @param bucketName The bucket name
-     * @return the root URL where the bucket name is located.
-     */
+   * Returns the root URL where the bucket name is located.
+   * <p>
+   * Please note that the URL does not contain the bucket name
+   * </p>
+   * 
+   * @param bucketName The bucket name
+   * @return the root URL where the bucket name is located.
+   * @throws S3Exception If something goes wrong.
+   */
     private String ensureBucketExists(String bucketName) {
 
         String bucketUrl = null;
@@ -111,6 +119,7 @@ public class S3Service {
         } catch (AmazonClientException ace) {
             LOG.error("An error occurred while connecting to S3. Will not execute action" +
                     " for bucket: {}", bucketName, ace);
+      throw new S3Exception(ace);
         }
 
 
@@ -118,19 +127,20 @@ public class S3Service {
     }
 
     /**
-     * It stores the given file name in S3 and returns the key under which the file has been stored
-     * @param resource The file resource to upload to S3
-     * @return The URL of the uploaded resource or null if a problem occurred
-     *
-     * @throws IllegalArgumentException If the resource file does not exist
-     */
+   * It stores the given file name in S3 and returns the key under which the file has been stored
+   * 
+   * @param resource The file resource to upload to S3
+   * @return The URL of the uploaded resource or null if a problem occurred
+   *
+   * @throws S3Exception If the resource file does not exist
+   */
     private String storeProfileImageToS3(File resource, String username) {
 
         String resourceUrl = null;
 
         if (!resource.exists()) {
             LOG.error("The file {} does not exist. Throwing an exception", resource.getAbsolutePath());
-            throw new IllegalArgumentException("The file " + resource.getAbsolutePath() + " doesn't exist");
+      throw new S3Exception("The file " + resource.getAbsolutePath() + " doesn't exist");
         }
 
         String rootBucketUrl = this.ensureBucketExists(bucketName);
@@ -153,6 +163,7 @@ public class S3Service {
             } catch (AmazonClientException ace) {
                 LOG.error("A client exception occurred while trying to store the profile" +
                         " image {} on S3. The profile image won't be stored", resource.getAbsolutePath(), ace);
+        throw new S3Exception(ace);
             }
         }
 
--- devopsbuddy/src/test/java/com/devopsbuddy/test/integration/StripeIntegrationTest.java
+++ devopsbuddy/src/test/java/com/devopsbuddy/test/integration/StripeIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.devopsbuddy.test.integration;
 
-import com.devopsbuddy.DevopsbuddyApplication;
-import com.devopsbuddy.backend.service.StripeService;
-import com.devopsbuddy.enums.PlansEnum;
-import com.stripe.Stripe;
-import com.stripe.model.Customer;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,14 +19,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.time.Clock;
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
+import com.devopsbuddy.DevopsbuddyApplication;
+import com.devopsbuddy.backend.service.StripeService;
+import com.devopsbuddy.enums.PlansEnum;
+import com.devopsbuddy.utils.StripeUtils;
+import com.stripe.Stripe;
+import com.stripe.model.Customer;
 
 /**
  * Created by tedonema on 13/05/2016.
@@ -57,11 +59,12 @@ public class StripeIntegrationTest {
 
         Map<String, Object> tokenParams = new HashMap<String, Object>();
         Map<String, Object> cardParams = new HashMap<String, Object>();
-        cardParams.put("number", TEST_CC_NUMBER);
-        cardParams.put("exp_month", TEST_CC_EXP_MONTH);
-        cardParams.put("exp_year", LocalDate.now(Clock.systemUTC()).getYear() + 1);
-        cardParams.put("cvc", TEST_CC_CVC_NBR);
-        tokenParams.put("card", cardParams);
+    cardParams.put(StripeUtils.STRIPE_CARD_NUMBER_KEY, TEST_CC_NUMBER);
+    cardParams.put(StripeUtils.STRIPE_EXPIRY_MONTH_KEY, TEST_CC_EXP_MONTH);
+    cardParams.put(StripeUtils.STRIPE_EXPIRY_YEAR_KEY,
+        LocalDate.now(Clock.systemUTC()).getYear() + 1);
+    cardParams.put(StripeUtils.STRIPE_CVC_KEY, TEST_CC_CVC_NBR);
+    tokenParams.put(StripeUtils.STRIPE_CARD_KEY, cardParams);
 
         Map<String, Object> customerParams = new HashMap<String, Object>();
         customerParams.put("description", "Customer for test@example.com");
